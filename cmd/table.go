4e2e693d34234bfd04aabfeb403927c5f9c5207d
linbaozhong <lin.baozhong@hotmail.com>
2020-08-19 16:47:34 +0800

    123

--- cmd/table.go
+++ cmd/table.go
@@ -4,7 +4,9 @@ var (
 	tableName = "TableName"
 	tableTpl  = `
 		package table
-
+		import (
+			"strings"
+		)
 		type _{{.StructName}} struct {
 			TableName string
 		{{range $key, $value := .Columns}} {{ $key }} TableField 
@@ -24,6 +26,20 @@ var (
 			Table: {{$.StructName}}.TableName,
 		} 
 		{{end}}
+		}
+
+		func (*_{{.StructName}}) Select(fields ...TableField) string {
+			if len(fields) == 0 {
+				return ""
+			}
+			buf := strings.Builder{}
+			for i, f := range fields {
+				if i > 0 {
+					buf.WriteByte(',')
+				}
+				buf.WriteString(f.Quote())
+			}
+			return buf.String()
 		}
 		`
 )
