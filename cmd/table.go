38f759dd536f1c2d6c062e7fe22f81d45fc3ca45
linbaozhong <64188059@qq.com>
2021-07-23 12:11:56 +0800

    sqlbuilder

--- cmd/table.go
+++ cmd/table.go
@@ -9,6 +9,7 @@ var (
 
 		type _{{.StructName}} struct {
 			TableName string
+			PrimaryKey TableField
 		{{range $key, $value := .Columns}} {{ $key }} TableField 
 		{{end}}
 		}
@@ -19,6 +20,12 @@ var (
 
 		func init() {
 			{{.StructName}}.TableName = "{{lower .TableName}}"
+
+			{{.StructName}}.PrimaryKey = TableField{
+				Name: "{{index .PrimaryKey 0}}",
+				Json: "{{index .PrimaryKey 1}}",
+				Table: {{$.StructName}}.TableName,
+			}
 		{{range $key, $value := .Columns}} 
 		{{ $.StructName}}.{{$key}} = TableField{
 			Name: "{{index $value 0}}",
--- tests/User.go
+++ tests/User.go
@@ -1,9 +1,12 @@
 package tests
 
 //tablename user
+//cachedata time.Minute *  1
+//  cachelist time.Minute * 1
+//cachelimit   500
 type User struct {
-	Name   string `json:"name"`
+	Name   string `json:"name" xorm:"'name' pk"`
 	Depart string `json:"depart"`
-	Age    int    `json:"age"`
+	Age    int    `json:"age" xorm:"'age'"`
 	IS     bool   `json:"is"`
 }
